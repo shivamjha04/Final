085fe12306230ae3439defb6fbb62815cbfdb074
shivamjha04 <34072255+shivamjha04@users.noreply.github.com>
2022-06-01 16:33:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          final update 4.0
+          latest
         </p>
         <a
           className="App-link"
